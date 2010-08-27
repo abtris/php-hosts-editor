e3ac56eca8cc9467ed549771370189fdc59ba0c9
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-08-27 13:35:02 +0200

    Add wireframes Try parse hosts file

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,21 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        // parse hosts file
+        $hostsFile = file_get_contents('/etc/hosts');
+        $lines = explode("\n", $hostsFile);
+        echo "<pre>";
+        $out = array();
+        foreach ($lines as $line) {
+//            echo $line."\n";
+            preg_match('/^(#?\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)\s(.*)/', $line, $matches);            
+            if (!empty($matches[1])) {
+                $out[$matches[1]] = trim($matches[2]);
+            }
+        }
+        
+        print_r($out);
+        
     }
 
 
